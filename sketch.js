3340846664050b49f62eed82130848c0624f15bf
AtipriyaSengupta <87596693+AtipriyaSengupta@users.noreply.github.com>
2021-07-25 12:01:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+ // background(30);
+if(keyDown(UP_ARROW)){
+  background("red")
+  
+}
+if(keyDown(DOWN_ARROW)){
+  background("yellow")
+}
+if(keyDown(RIGHT_ARROW)){
+  background("black")
+  
+}
+if(keyDown(LEFT_ARROW)){
+  background("beige")
+}
+}
+
+
+
+
